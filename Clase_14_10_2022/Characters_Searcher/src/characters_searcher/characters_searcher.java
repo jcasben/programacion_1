7d7a38aa662599c15d1362afed7450b1a0713aa0
Jesus Castillo <jcasben02@gmail.com>
2022-10-14 11:32:53 +0200

    Agregando los progrmas hechos en las clases 07/10 y 14/10

---
+++ Clase_14_10_2022/Characters_Searcher/src/characters_searcher/characters_searcher.java
@@ -0,0 +1,49 @@
+package characters_searcher;
+
+public class characters_searcher {
+    
+    private void inicio(){
+    
+        Character letter;
+        LT in =  new LT();
+        Boolean validation = false;
+        
+        System.out.println("Introduce una letra");
+        letter = in.llegirCaracter();
+        
+        while(letter == null){
+            System.out.println("ERROR: lo que has introducido no es una letra");
+            letter = in.llegirCaracter();
+        }
+        
+        while ((!validation) && (letter != '.')){
+                        
+            while(letter ==  null){
+                System.out.println("ERROR: lo que has introducido no es una letra");
+                letter = in.llegirCaracter();
+            }
+            
+            System.out.println("Introduce una letra");
+            letter = in.llegirCaracter();
+            while(letter ==  null){
+                System.out.println("ERROR: lo que has introducido no es una letra");
+                letter = in.llegirCaracter();
+            }
+            
+            if ((letter == 'a') || (letter == 'e') || (letter == 'i') || (letter == 'o') || (letter == 'u')){
+                validation = true;
+            }
+        }
+        if (letter == '.'){
+            System.out.println("No se han encontrado la letra a");
+        }else{
+            System.out.println("Se ha encontrado una vocal minúscula, la vocal '" + letter + "'");
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        
+        (new characters_searcher()).inicio();
+    }   
+}

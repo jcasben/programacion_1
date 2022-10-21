a510d5e907405a55830b0753ba3482fda99f9fc4
Jesus Castillo <jcasben02@gmail.com>
2022-10-21 09:07:41 +0200

    Creación del programa LAS_searcher.

---
+++ Clase_21_10_2022/LAS_searcher/src/LAS_searcher/Auxiliar_funct.java
@@ -0,0 +1,36 @@
+package LAS_searcher;
+
+public class Auxiliar_funct {
+
+    LT in = new LT();
+
+    public Auxiliar_funct(){
+        
+    }
+    
+    public String find_LAS(){
+
+        Character letter = ' ', previous_letter = ' ', previous_letter2 = ' ';
+        int counter = 0;
+
+        System.out.println("Por favor, introduce carácteres y yo te diré cuantos grupos de 'las' has introducido.");
+
+        while (letter != '.'){
+
+            System.out.println("Introduce un carácter.");
+            if ((previous_letter2 == 'l') && (previous_letter == 'a') && (letter == 's')){
+                counter++;
+            }
+
+            previous_letter2 = previous_letter;
+            previous_letter = letter;
+
+            letter = in.llegirCaracter();
+            while(letter ==  null){
+                System.out.println("ERROR: lo que has introducido no es un carácter");
+                letter = in.llegirCaracter();
+            }
+        }
+        return "Has encontrado " + counter + " grupos de 'LAS'";
+    }
+}
